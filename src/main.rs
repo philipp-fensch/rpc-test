f613ea1fa3039b9d7031ce1382b22ceef7175d83
Philipp Fensch <philipp.fensch@rwth-aachen.de>
2021-09-29 16:07:17 +0200

    Changed code according to changes in rpc-lib

--- src/main.rs
+++ src/main.rs
@@ -5,74 +5,57 @@ use rpc_lib::include_rpcl;
 struct RPCConnection;
 
 fn main() {
+    cuda_solve_linear_system();
 }
 
-#[cfg(test)]
-mod test {
+use std::mem::size_of;
 
-    use super::*;
-    use std::mem::size_of;
-
-    #[test]
-    fn init_test() {
-        let _rpc_connection = RPCConnection::new("127.0.0.1");
-    }
-
-    // TODO: Integration-Test!
-    #[test]
-    fn cuda_malloc_free() {
-        let rpc_connection = RPCConnection::new("127.0.0.1");
-        let id = rpc_connection.cuda_malloc(8);
-        assert!(id.err == 0, "cuda_malloc failed");
-        rpc_connection.cuda_free(id.ptr_result_u);
-    }
-
-    #[test]
-    fn cuda_solve_linear_system() {
+fn cuda_solve_linear_system() {
+        const DIM: usize = 3;
         // System to Solve: (transposed)
         // | 2 2 0 |       | 2 |               |-1 |
         // | 0 2 0 | * X = | 4 | Solution: X = | 2 |
         // | 0 1 1 |       | 5 |               | 3 |
-        let mut matrix_host: [f64; 3 * 3] = [
+        let matrix_host: [f64; DIM * DIM] = [
             2.0, 0.0, 0.0, // transposed
             2.0, 2.0, 1.0,
-            0.0, 0.0, 1.0 
+            0.0, 0.0, 1.0
         ];
-        let mut right_side_host: [f64; 3] = [
+        let right_side_host: [f64; DIM] = [
             2.0,
             4.0,
             5.0
         ];
 
+        // Cast f64 to u8 for cudamemcpy
+        let matrix_host_cast = unsafe {
+            std::mem::transmute::<[f64; DIM * DIM], [u8; size_of::<f64>() * DIM * DIM]>(matrix_host)
+        };
+
         // Init Connection and CUDA
-        let rpc_connection = RPCConnection::new("127.0.0.1");
-        let solver = rpc_connection.rpc_cusolverdncreate();
+        let rpc_connection = RPCConnection::new("137.226.133.199");
+        let solver = rpc_connection.rpc_cusolverdncreate().unwrap();
 
         // Allocate Memory
-        let (vector_device, matrix_device, piv_seq_device, err, workspace) = allocate_memory(&rpc_connection, &solver);
+        let (vector_device, matrix_device, piv_seq_device, err, workspace) = allocate_memory(&rpc_connection, solver);
 
         // Copy Matrix to Device
-        let matrix_ptr_host = mem_data {
-            len: 3 * 3 * size_of::<f64>() as u32,
-            data: matrix_host.as_mut_ptr() as *mut c_void
-        };
-        rpc_connection.cuda_memcpy_htod(matrix_device, matrix_ptr_host, 3 * 3 * size_of::<f64>() as u64);
+        rpc_connection.cuda_memcpy_htod(matrix_device, matrix_host_cast.to_vec(), matrix_host_cast.len() as u64);
 
         // LU
-        lu_factorization(&rpc_connection, &solver, matrix_device, workspace, piv_seq_device, err);
+        lu_factorization(&rpc_connection, solver, matrix_device, workspace, piv_seq_device, err);
         rpc_connection.cuda_device_synchronize();
 
         // Solve System
         // Copy rhs to Device
-        let right_side_ptr_host = mem_data {
-            len: 3 * size_of::<f64>() as u32,
-            data: right_side_host.as_mut_ptr() as *mut c_void
+        let right_side_host_cast = unsafe {
+            std::mem::transmute::<[f64; DIM], [u8; size_of::<f64>() * DIM]>(right_side_host)
         };
-        rpc_connection.cuda_memcpy_htod(vector_device, right_side_ptr_host, 3 * size_of::<f64>() as u64);
+        rpc_connection.cuda_memcpy_htod(vector_device, right_side_host_cast.to_vec(), right_side_host_cast.len() as u64);
 
         // Solve
         let res = rpc_connection.rpc_cusolverdndgetrs(
-            solver.ptr_result_u,
+            solver,
             0, // CUBLAS_OP_N
             3,
             1, //#right-hand-sides
@@ -84,19 +67,24 @@ mod test {
             err
         );
         rpc_connection.cuda_device_synchronize();
-        assert!(*res == 0, "Solving System failed (cusolverDnDgetrs)");
+        assert!(res == 0, "Solving System failed (cusolverDnDgetrs)");
 
         // Copy left-hand-side back
-        let res2 = rpc_connection.cuda_memcpy_dtoh(vector_device, 3 * size_of::<f64>() as u64);
-        let solution = unsafe { std::slice::from_raw_parts(res2.mem_result_u.data as *mut f64, 3) };
+        let res = rpc_connection.cuda_memcpy_dtoh(vector_device, (size_of::<f64>() * DIM) as u64);
+        let res2 = res.as_ref().unwrap();
 
+        // Cast mem_result from generic u8 to the actual f64
+        let solution = unsafe {
+            std::mem::transmute::<&[u8], &[f64]>(&res2)
+        };
+        
         // Check Result
         assert!(
             (solution[0] + 1.0).abs() < 0.001 ||
             (solution[1] - 2.0).abs() < 0.001 ||
             (solution[2] - 3.0).abs() < 0.001,
             "Solution wrong"
-        ); 
+        );
 
         // Free Memory
         rpc_connection.cuda_free(vector_device);
@@ -105,36 +93,36 @@ mod test {
         rpc_connection.cuda_free(err);
         rpc_connection.cuda_free(workspace);
 
-        rpc_connection.rpc_cusolverdndestroy(solver.ptr_result_u);
+        assert!(rpc_connection.rpc_cusolverdndestroy(solver) == 0, "rpc_cusolverdndestroy failed");
     }
 
-    fn allocate_memory(rpc_connection: &RPCConnection, solver: &ptr_result) -> (u64, u64, u64, u64, u64) {
+    fn allocate_memory(rpc_connection: &RPCConnection, solver: u64) -> (u64, u64, u64, u64, u64) {
         // Vector
-        let rhs_vector = rpc_connection.cuda_malloc(3 * size_of::<f64>() as u64).ptr_result_u;
+        let rhs_vector = rpc_connection.cuda_malloc(3 * size_of::<f64>() as u64).unwrap();
         // Matrix
-        let mat = rpc_connection.cuda_malloc(3 * 3 * size_of::<f64>() as u64).ptr_result_u;
+        let mat = rpc_connection.cuda_malloc(3 * 3 * size_of::<f64>() as u64).unwrap();
         // Pivot
-        let piv = rpc_connection.cuda_malloc(3 * size_of::<f64>() as u64).ptr_result_u;
+        let piv = rpc_connection.cuda_malloc(3 * size_of::<f64>() as u64).unwrap();
         // Error-Code
-        let err = rpc_connection.cuda_malloc(size_of::<i32>() as u64).ptr_result_u;
+        let err = rpc_connection.cuda_malloc(size_of::<i32>() as u64).unwrap();
 
         let workspace_size = rpc_connection.rpc_cusolverdndgetrf_buffersize(
-            solver.ptr_result_u,
+            solver,
             3,
             3,
             mat,
             3
-        );
-        assert!(workspace_size.err == 0, "cusolverdndgetrf_buffersize failed");
+        ).unwrap();
+        // assert!(workspace_size == 0, "cusolverdndgetrf_buffersize failed");
 
-        let workspace = rpc_connection.cuda_malloc(workspace_size.int_result_u as u64).ptr_result_u;
+        let workspace = rpc_connection.cuda_malloc(workspace_size as u64).unwrap();
 
         (rhs_vector, mat, piv, err, workspace)
     }
 
-    fn lu_factorization(rpc_connection: &RPCConnection, solver: &ptr_result, matrix: u64, workspace: u64, piv: u64, err: u64) {
+    fn lu_factorization(rpc_connection: &RPCConnection, solver: u64, matrix: u64, workspace: u64, piv: u64, err: u64) {
         let res = rpc_connection.rpc_cusolverdndgetrf(
-            solver.ptr_result_u,
+            solver,
             3,
             3,
             matrix,
@@ -143,6 +131,5 @@ mod test {
             piv,
             err
         );
-        assert!(*res == 0, "LU-Factorization failed (cusolverDnDgetrf)");
+        assert!(res == 0, "LU-Factorization failed (cusolverDnDgetrf)");
     }
-}
